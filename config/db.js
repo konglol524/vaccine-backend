6c4c0ae380f943529ff18d9ce00290bbb306df97
konglol524 <kopmul26@gmail.com>
2024-02-20 14:40:35 +0700

    connect with mongoDB

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://kongbhumi:kongbhumi123@swpracticecluster.wqpltih.mongodb.net/vacQ?retryWrites=true&w=majority";
+const connectDB = async () => {
+    mongoose.set('strictQuery', true); //to suppress deprecated warning
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+}
+
+// const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+// async function connectDB() {
+//   try {
+//     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+//     await mongoose.connect(uri, clientOptions);
+//     await mongoose.connection.db.admin().command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await mongoose.disconnect();
+//   }
+// }
+// connectDB().catch(console.dir);
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
-const app=express();
+//connect to database
+connectDB();
 
-// if user GET /, do this
-// app.get('/', (req, res)=> {
-//     //1. res.send('<h1>Hello from express</h1>');
-//     //2. res.send({name:'Brad'});
-//     //3. res.json({name:'Brad'});
-//     //4. res.sendStatus(400);
-//     //5. res.status(400).json({success:false});
-//     res.status(200).json({success:true, data:{id:1}});
-// });
+const app=express();
+//add body parser
+app.use(express.json());
 
 const hospitals = require('./routes/hospitals');
 app.use('/api/v1/hospitals', hospitals);
 
 const PORT=process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //close server and exit process
+    server.close(()=>process.exit(1));
+});
