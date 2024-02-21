15bbb084838bb917657443ee1dd60eb02f11ff2f
konglol524 <kopmul26@gmail.com>
2024-02-21 13:59:03 +0700

    added auth.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 //Load env vars
@@ -12,8 +13,15 @@ const app=express();
 //add body parser
 app.use(express.json());
 
+//Cookie parser
+app.use(cookieParser());
+
+//router files
 const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
+
 app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth', auth)
 
 const PORT=process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
